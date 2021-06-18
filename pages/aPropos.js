6420965f442d2f20ea5d7efbdee039cf3be4a195
Rebecca <rebeccadumazert@hotmail.fr>
2021-06-18 19:28:37 +0200

    clean about

--- pages/aPropos.js
+++ pages/aPropos.js
@@ -1,18 +1,10 @@
 import Head from 'next/head'
 import { Header } from '../components/Header'
 import img from '../public/images/fond_dégradé_long.png'
-import picture from '../public/images/aPropos.jpg'
 import style from '../styles/APropos.module.css'
 
 function APropos() {
-  const {
-    container,
-    description,
-    intro,
-    blackLine,
-    pictureContainer,
-    pictureAndText,
-  } = style
+  const { container, description, intro, text } = style
   return (
     <>
       <Head>
@@ -24,11 +16,7 @@ function APropos() {
           Frange Radicale est une coopérative de coiffeur·se·s ayant pour but de
           rendre la coiffure plus inclusive, plus solidaire et plus écologique.
         </p>
-        {/* <div className={blackLine}></div> */}
-        <div className={pictureAndText}>
-          <div className={pictureContainer}>
-            <img src={picture} alt='' />
-          </div>
+        <div className={text}>
           <div>
             <p className={description}>
               En décembre 2020, nous avons ouvert Frange Radicale, une
